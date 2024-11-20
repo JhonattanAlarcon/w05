d6f0b502a34a70d7ca146d8b2f4cfd9c6ecb46ac
JhonattanAlarcon <jhonattan_alarcon@hotmail.com>
2024-11-19 23:12:41 -0500

    test2

--- WebApplication5/WebApplication5/Controllers/WeatherForecastController.cs
+++ WebApplication5/WebApplication5/Controllers/WeatherForecastController.cs
@@ -25,6 +25,7 @@ namespace WebApplication5.Controllers
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
+                Nombre ="Jhntn",
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
